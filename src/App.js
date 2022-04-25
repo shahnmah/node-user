1c886eb4aeba22d642261bd4696dc5965ab4ed09
shahnmah <shahn.mah@gmail.com>
2022-04-25 22:45:04 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect( ()=>{
+    fetch('http://localhost:5000/users/')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+  const handelAddUser = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name , email}
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My own Data {users.length}</h1>
+      <form onSubmit={handelAddUser}>
+        <input type="text" name="name" placeholder='Your Name' required/> <br />
+        <input type="email" name="email" placeholder='Your Email' required/> <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
     </div>
   );
 }
